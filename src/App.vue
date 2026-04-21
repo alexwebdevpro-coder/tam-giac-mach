227706717b0be84a6e75909a32d191cabc950d09
Alex.ZIRNHELT <alexis.zirnhelt@gmail.com>
2026-04-21 17:07:28 +0200

    chore: install and configure tailwindcss v4

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
 <template>
   <h1>You did it!</h1>
-  <p>
+  <p class="bg-red-500 text-white p-8">
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
